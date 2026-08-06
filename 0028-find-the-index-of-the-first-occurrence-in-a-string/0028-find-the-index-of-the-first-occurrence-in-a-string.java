d1c67479d50b5cea8f291947ccf2020a9a3aa37f
Arpan <145189346+Arpan-Multeli@users.noreply.github.com>
2026-08-06 22:32:16 +0530

    Time: 1 ms (48.81%), Space: 43.1 MB (52.22%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -6,7 +6,6 @@ class Solution {
 
         for(int i = 0 ; i <= n-m; i++){
             boolean found = true;
-
             for(int j =0; j<m;j++){
                 if(haystack.charAt(i + j) != needle.charAt(j)){
                     found = false;
